a9e36121ba90e37d88a5f409c413c630f0b99172
Vera Ndulue <verandulue1@gmail.com>
2023-07-28 00:02:15 +0100

    first-commit

--- Javascript/styles.js
+++ Javascript/styles.js
@@ -1,20 +1,15 @@
-// Get the theme toggle icon element
 const themeToggle = document.getElementById('theme-toggle');
 
-// Add a click event listener to the theme toggle icon
 themeToggle.addEventListener('click', function() {
-  // Get the <body> element
   const body = document.body;
   const hamburger = document.querySelector('.hamburger');
   const links = document.querySelector('.link-2');
   
 
-  // Toggle the "dark-theme" class on the body element
   body.classList.toggle('dark-theme');
   links.classList.toggle('dark-theme');
 hamburger.classList.toggle('dark-theme');
 
-  // Toggle the icon class between sun and moon
   themeToggle.firstElementChild.classList.toggle('fa-moon');
   themeToggle.firstElementChild.classList.toggle('fa-sun');
 });
